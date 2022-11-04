37c47917e1e2fe7d49d93520fcd4c73532d2cb5c
Gabriel <gabrielferrari601@gmail.com>
2022-11-04 15:19:19 -0300

    refactor(routes): refatora rota de content

--- src/routes/contentRoute.js
+++ src/routes/contentRoute.js
@@ -1,15 +1,37 @@
-const { Router } = require("express");
+// const { Router } = require("express");
+// const ContentController = require("../controllers/contentController");
+// const ContentService = require("../services/contentService");
+
+// const routes = new Router();
+// const contentService = new ContentService();
+// const contentController = new ContentController(contentService);
+
+// routes.get("/:track/:subtrack", (req, res) => contentController.show(req, res));
+// routes.post("/", (req, res) => contentController.create(req, res));
+// routes.put("/:id", (req, res) => contentController.edit(req, res));
+// routes.patch("/:id", (req, res) => contentController.update(req, res));
+// routes.delete("/:id", (req, res) => contentController.destroy(req, res));
 const ContentController = require("../controllers/contentController");
-const ContentService = require("../services/contentService");
+const CustomRouter = require("./Router");
 
-const routes = new Router();
-const contentService = new ContentService();
-const contentController = new ContentController(contentService);
+class ContentRouter extends CustomRouter {
+  constructor() {
+    super();
+    this.addController = this.addController.bind(this);
+  }
 
-routes.get("/:track/:subtrack", (req, res) => contentController.show(req, res));
-routes.post("/", (req, res) => contentController.create(req, res));
-routes.put("/:id", (req, res) => contentController.edit(req, res));
-routes.patch("/:id", (req, res) => contentController.update(req, res));
-routes.delete("/:id", (req, res) => contentController.destroy(req, res));
+  addController(controller, route = controller.route) {
+    this.router.post(route, controller.create);
+    this.router.get(`${route}/:track/:subtrack`, controller.read);
+    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.put(`${route}/:id`, controller.update);
+    this.router.delete(`${route}/:id`, controller.delete);
+  }
+}
 
-module.exports = routes;
+const contentController = new ContentController();
+const contentRouter = new ContentRouter();
+
+contentRouter.addController(contentController);
+
+module.exports = contentRouter.router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv/config");
-
+const contentRoute = require("./routes/contentRoute");
 const App = require("./api");
 
 const server = new App();
-
+server.addRouter(contentRoute);
 server.start();
