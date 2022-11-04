0247b663a5c113246cbad4470b1d762cdd915a0f
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-04 12:52:00 -0300

    fix(connection): add database name to URI

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-const BASE_URL = "mongodb://localhost:27017";
+const BASE_URL = "mongodb://localhost:27017/orangeEvolution";
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI || BASE_URL,
