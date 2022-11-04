1cda50fca57d5581a13d308e648c09d8d1d980ff
Gabriel <gabrielferrari601@gmail.com>
2022-11-04 16:37:48 -0300

    feat(middleWares): :zap: adiciona middleware de tratamento de erros

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 require("dotenv/config");
+require("express-async-errors");
 const contentRoute = require("./routes/contentRoute");
 const App = require("./api");
 
 const server = new App();
 
 server.addRouter(contentRoute);
+server.useErrorMiddleware();
 server.start();
