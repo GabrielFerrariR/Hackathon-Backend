48fca8609c8e808f0448f2aeb7d0255b848c0004
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-04 21:02:20 -0300

    feat: validate username and email are unique

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -12,6 +12,17 @@ const UserSchema = new Schema({
 class UserModel extends Model {
   constructor(model = createModel("Users", UserSchema)) {
     super(model);
+
+    this.findUsername = this.findUsername.bind(this);
+    this.findEmail = this.findEmail.bind(this);
+  }
+
+  async findUsername(username) {
+    return this.model.findOne({ username });
+  }
+
+  async findEmail(email) {
+    return this.model.findOne({ email });
   }
 }
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,5 @@
 const Service = require(".");
+const Conflict = require("../errors/Conflict");
 const UserModel = require("../models/UserModel");
 const { validateUserRegistration } = require("./validations/responses");
 
@@ -8,11 +9,31 @@ class UserService extends Service {
     this.model = new UserModel();
 
     this.create = this.create.bind(this);
+    this.validateUniqueUsername = this.validateUniqueUsername.bind(this);
+    this.validateUniqueEmail = this.validateUniqueEmail.bind(this);
+  }
+
   async create(data) {
+    const { username, email } = data;
 
     validateUserRegistration(data);
 
+    await this.validateUniqueUsername(username);
+    await this.validateUniqueEmail(email);
+
     return this.model.create(data);
+  }
+
+  async validateUniqueUsername(username) {
+    const doesUsernameExist = await this.model.findUsername(username);
+
+    if (doesUsernameExist) throw new Conflict("Username already in use");
+  }
+
+  async validateUniqueEmail(email) {
+    const doesEmailExist = await this.model.findEmail(email);
+
+    if (doesEmailExist) throw new Conflict("Email already in use");
   }
 }
 
