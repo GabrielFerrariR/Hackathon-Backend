d7e3d0ae89eb3e5a0b8b3b624e96c2f88499d4c3
Gabriel <gabrielferrari601@gmail.com>
2022-11-08 19:55:49 -0300

    fix: :bug: corrrige nome da funçao readOne

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,7 +11,7 @@ class UserController extends Controller {
 
     this.create = super.create.bind(this);
     this.read = super.read.bind(this);
-    this.readOne = super.readOne.bind(this);
+    this.readOne = super.readById.bind(this);
     this.update = super.update.bind(this);
     this.delete = super.delete.bind(this);
     this.toggleCompletedContent = this.toggleCompletedContent.bind(this);
---
+++ tests/mocks/userMock.js
@@ -0,0 +1,15 @@
+module.exports = [{
+  "username": "Jhondoe",
+  "password": "ea84sdad51",
+  "name": "Jhon Doe",
+  "email": "johnaa@gmail.com",
+  "completedContents": [],
+  "_id": "636ab5b6591b4afd43a66f75",
+  "__v": 0
+},
+{
+  "username": "Jhondoe",
+  "password": "ea84sdad51",
+  "name": "Jhon Doe",
+  "email": "johnaa@gmail.com",
+}]
