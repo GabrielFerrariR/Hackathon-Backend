25c629f843bd085e4444ad46c8c0f229793a54c7
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-09 14:11:56 -0300

    refactor: remove username from user model

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -3,7 +3,6 @@ const Joi = require("joi");
 const idSchema = Joi.number().integer().min(1).required();
 
 const userRegistrationSchema = Joi.object({
-  username: Joi.string().min(3).required(),
   password: Joi.string().min(8).required(),
   name: Joi.string().required(),
   email: Joi.string().email().required(),
