479c3ef9175d79d504c9c7e2553cc16d084f3823
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-11 18:39:24 -0300

    test: add login controller tests

---
+++ tests/unit/controllers/LoginController.test.js
@@ -0,0 +1,38 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const sinon = require("sinon");
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const LoginController = require("../../../src/controllers/LoginController");
+const [userMockDBResponse, _, loginMockRequest] = require("../../mocks/userMock");
+
+describe("Login controller class", () => {
+  const controller = new LoginController();
+  const req = {};
+  const res = {};
+  const next = () => {};
+
+  describe("create method, on a successful request", () => {
+    beforeEach(async () => {
+      req.body = loginMockRequest;
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      sinon.stub(controller.service, "validateLogin").resolves(userMockDBResponse);
+    });
+
+    afterEach(() => sinon.restore());
+
+    it("should return a status 200", async () => {
+      await controller.create(req, res, next);
+      expect(res.status.calledWith(200)).to.be.true;
+    });
+
+    it("should return an user", async () => {
+      await controller.create(req, res, next);
+      expect(res.json.calledWith(userMockDBResponse)).to.be.true;
+    });
+  });
+});
