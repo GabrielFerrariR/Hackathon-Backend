4520887ed17e0225aca81b1a3f8b81ed8885fbf0
alvesgf16 <alvesgabriel1994@gmail.com>
2022-11-11 18:05:46 -0300

    test: change mocks to reflect reality better

--- tests/mocks/userMock.js
+++ tests/mocks/userMock.js
@@ -1,24 +1,18 @@
-module.exports = [{
-  "username": "Jhondoe",
-  "password": "ea84sdad51",
-  "name": "Jhon Doe",
-  "email": "johnaa@gmail.com",
-  "completedContents": [1, 2],
-  "_id": "636ab5b6591b4afd43a66f75",
-  "__v": 0
-},
-{
-  "username": "Jhondoe",
-  "password": "ea84sdad51",
-  "name": "Jhon Doe",
-  "email": "johnaa@gmail.com",
-},
-{
-  "username": "Jhondoe",
-  "password": "ea84sdad51",
-  "name": "Jhon Doe",
-  "email": "johnaa@gmail.com",
-  "completedContents": [1, 2],
-  "_id": "636ab5b6591b4afd43a66f75",
-  "__v": 0
-}]
+module.exports = [
+  {
+    name: "Jhon Doe",
+    email: "johnaa@gmail.com",
+    completedContents: [1, 2],
+    _id: "636ab5b6591b4afd43a66f75",
+    __v: 0,
+  },
+  {
+    password: "ea84sdad51",
+    name: "Jhon Doe",
+    email: "johnaa@gmail.com",
+  },
+  {
+    password: "ea84sdad51",
+    email: "johnaa@gmail.com",
+  },
+];
